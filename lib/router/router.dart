ce04bb6b61e3603f89afc5dd8b3670659fcf1fcf
Михаил Лукавский <lukavskym@ya.ru>
2024-08-02 12:33:25 +0300

    Добавлен роут на страницу редактирования контакта

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,9 @@
+import 'package:my_todo_app/features/contact_edit/view/contact_edit_page.dart';
 import 'package:my_todo_app/features/contact_item/view/view.dart';
 import 'package:my_todo_app/features/contacts_list/view/view.dart';
 
 final routes = {
   '/': (context) => const AllContactsScreen(title: "ContactsApp"),
   '/contact': (context) => const ContactScreen(),
+  '/editcontact': (context) => const ContactEditPage(),
 };
